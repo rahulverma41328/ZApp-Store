d4c6aa1f6b68314b4a50cafee3613976d74d56a9
Rahul Verma <rahulverma@Rahuls-Air.lan>
2024-11-10 20:27:39 +0530

    rahul verma

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id ("kotlin-parcelize")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("dagger.hilt.android.plugin")
+    kotlin("kapt")
 }
 
 android {
@@ -50,4 +54,43 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    //loading button
+    implementation("com.github.leandroborgesferreira:loading-button-android:2.3.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    //circular image
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    //viewpager2 indicator
+    // implementation ("io.github.vejei.viewpagerindicator:viewpagerindicator:1.0.0-alpha.1")
+
+    //stepView
+
+    //Android Ktx
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.3")
+
+    //Dagger hilt
+    implementation ("com.google.dagger:hilt-android:2.52")
+    kapt ("com.google.dagger:hilt-compiler:2.52")
+
+    //Firebase
+    implementation ("com.google.firebase:firebase-auth:23.1.0")
+
+    //Coroutines with firebase
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
+
+    // implementation("com.github.shuhart:stepview:1.5.1")
+
+    //pin view
+    implementation ("io.github.chaosleung:pinview:1.4.4")
+
+    // country code picker// https://mvnrepository.com/artifact/com.hbb20/CountryCodePicker
+    //implementation("com.hbb20:CountryCodePicker:2.4.7")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation ("com.google.firebase:firebase-appcheck-playintegrity:18.0.0")
 }
